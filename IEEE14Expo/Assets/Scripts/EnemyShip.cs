fbe97e5d406446f8a88849f2d07afc1b58afa2a8
Manuel Esquer Cerezo <manuelsker@gmail.com>
2014-10-13 18:52:09 +0200

    Destruction base added

---
+++ IEEE14Expo/Assets/Scripts/EnemyShip.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class EnemyShip : MonoBehaviour,IDestructible
+{
+	public float Life {
+				get;
+				private set;
+
+	}
+
+	public void TakeDamage (float a_damage)
+	{
+		Life -= a_damage;
+
+		if (Life < 0) 
+		{ 
+			if (OnDeath != null)
+				OnDeath (this, null);
+
+		}
+	}
+
+	public event EventHandler OnDeath;
+
+}
---
+++ IEEE14Expo/Assets/Scripts/IDestructible.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public interface IDestructible
+{ 
+	float Life {
+				get; 
+	}
+
+	void TakeDamage(float a_damage);
+
+
+	event EventHandler OnDeath;
+}
